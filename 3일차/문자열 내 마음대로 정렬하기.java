b86315c9b6baece2caaa79756686ff1205e1b92e
css04146 <70522531+css04146@users.noreply.github.com>
2023-10-18 13:27:00 +0900

    Update 문자열 내 마음대로 정렬하기.java

--- 3일차/문자열 내 마음대로 정렬하기.java
+++ 3일차/문자열 내 마음대로 정렬하기.java
@@ -14,3 +14,17 @@ class Solution {
         return answer;
     }
 }
+
+//..........................................
+
+import java.util.Arrays;
+class Solution {
+    public String[] solution(String[] strings, int n) {
+        Arrays.sort(strings, (o1, o2) -> {
+            if(o1.charAt(n) < o2.charAt(n)) return -1;
+            else if(o1.charAt(n) == o2.charAt(n)) return o1.compareTo(o2);
+            else return 1;
+        });
+        return strings;
+    }
+}
