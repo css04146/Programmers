466a6f9e47aa0fb09252e98eb62c09c81c13e2e2
css04146 <70522531+css04146@users.noreply.github.com>
2023-10-25 18:35:40 +0900

    Update JadenCase 문자열 만들기.java

--- 8일차/JadenCase 문자열 만들기.java
+++ 8일차/JadenCase 문자열 만들기.java
@@ -12,3 +12,16 @@ class Solution {
         return sb.toString();
     }
 }
+//
+class Solution {
+    public String solution(String s) {
+        int index = 0;
+        s = s.toLowerCase();
+        char[] c = s.toCharArray();
+        for(int i = 0; i < c.length; i++){
+            c[i] = index == 0 ? Character.toUpperCase(c[i]) : c[i];
+            index = c[i] == ' ' ? 0 : index + 1;
+        }
+        return new StringBuilder(new String(c)).toString();
+    }
+}
