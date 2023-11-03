d697ece84c702bf0cb2f8bff13781dd58e980708
css04146 <70522531+css04146@users.noreply.github.com>
2023-11-03 10:22:27 +0900

    Create 문자열 나누기.java

---
+++ 12일차/문자열 나누기.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        int c_count = 0; // 목표되는 c의 개수
+        int o_count = 0; // 그 이외의 개수
+        char c = s.charAt(0); // 목표가 되는 c
+        for(int i = 0; i < s.length(); i++){ // 한 자리면 i = 1이 될 수 없다. 
+            if(c_count == 0) c = s.charAt(i);
+            if(s.charAt(i) == c) c_count++;
+            else o_count++;
+            if(c_count == o_count){
+                c_count = 0;
+                o_count = 0;
+                answer++;
+            }
+            if(i == s.length() - 1 && c_count != o_count){
+                answer++;
+                break;
+            }
+        }
+        return answer;
+    }
+}
