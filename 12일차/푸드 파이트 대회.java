3cc75a44017e355648fab69b73465540cb27ba5f
css04146 <70522531+css04146@users.noreply.github.com>
2023-11-03 10:30:01 +0900

    Create 푸드 파이트 대회.java

---
+++ 12일차/푸드 파이트 대회.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(int[] food) {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 1; i < food.length; i++){
+            int len = food[i] / 2;
+            for(int j = 0; j < len; j++){
+                sb.append(i);
+            }
+        }
+        return sb.toString() + "0" + sb.reverse().toString();
+    }
+}
