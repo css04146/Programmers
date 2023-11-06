fbe6879e006057d7f3695d7b494f891129bc4d06
css04146 <70522531+css04146@users.noreply.github.com>
2023-11-06 16:57:19 +0900

    Create 옹알이 (2).java

---
+++ 13일차/옹알이 (2).java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(String[] babbling) {
+        int answer = 0;
+        for(String b : babbling){
+            if(b.contains("woowoo")) continue;
+            if(b.contains("ayaaya")) continue;
+            if(b.contains("yeye")) continue;
+            if(b.contains("mama")) continue;
+            b = b.replace("aya", " ");
+            b = b.replace("woo", " ");
+            b = b.replace("ye", " ");
+            b = b.replace("ma", " ");
+            b = b.replace(" ", "");
+            if(b.equals("")) answer++;
+        }
+        return answer;
+    }
+}
