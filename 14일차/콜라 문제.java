e9a3bfc86cb21aec4217a6938274da3da7673b88
css04146 <70522531+css04146@users.noreply.github.com>
2023-11-07 16:59:12 +0900

    Create 콜라 문제.java

---
+++ 14일차/콜라 문제.java
@@ -0,0 +1,11 @@
+// 복습 필요
+class Solution {
+    public int solution(int a, int b, int n) {
+        int answer = 0;
+        while(a <= n){
+            answer += n / a * b;
+            n = n / a * b + n % a;
+        }
+        return answer;
+    }
+}
