e916c058def27808dc169d41e5fefab0a39ca4db
css04146 <70522531+css04146@users.noreply.github.com>
2023-11-16 15:55:18 +0900

    Update 기능개발.java

--- 3주차/21일차/기능개발.java
+++ 3주차/21일차/기능개발.java
@@ -12,3 +12,47 @@ class Solution {
     }
 }
 //
+import java.util.List;
+import java.util.ArrayList;
+class Solution {
+    class Task{
+        int progress;
+        int speed;
+        Task(int progress, int speed){
+            this.progress = progress;
+            this.speed = speed;
+        }
+        void makeProgress(){
+            this.progress += this.speed;
+        }
+        boolean isComplete(){
+            return this.progress >= 100;
+        }
+    }
+    public int[] solution(int[] progresses, int[] speeds) {
+        List<Task> taskList = new ArrayList<>();
+        List<Integer> output = new ArrayList<>();
+        
+        for(int i = 0; i < speeds.length; i++){
+            taskList.add(new Task(progresses[i], speeds[i]));
+        }
+        
+        while(!taskList.isEmpty()){
+            for(Task task : taskList){
+                task.makeProgress();
+            }
+            
+            int cnt = 0;
+            for(int i = 0; i < taskList.size(); i++){
+                if(taskList.get(i).isComplete()){
+                    cnt++;
+                    taskList.remove(i--);
+                }
+                else break;
+            }
+            if(cnt != 0) output.add(cnt);
+        }
+        
+        return output.stream().mapToInt(i -> i).toArray();
+    }
+}
