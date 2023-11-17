c93270905d40ce795380c8f7e24c00bf7199b7e5
css04146 <70522531+css04146@users.noreply.github.com>
2023-11-17 15:08:42 +0900

    Create 이진 변환 반복하기.java

---
+++ 4주차/22일차/이진 변환 반복하기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] solution(String s) {
+        int[] answer = new int[2];
+        while(!s.equals("1")){
+            int len = s.length();
+            s = s.replace("0", "");
+            int alen = s.length();
+            answer[0]++;
+            answer[1] += len - alen;
+            s = Integer.toString(alen, 2);
+        }
+        return answer;
+    }
+}
