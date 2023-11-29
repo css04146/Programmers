b4d00756d61d3b4e4f00a827ff0d8927a6b1c106
css04146 <70522531+css04146@users.noreply.github.com>
2023-11-29 17:50:02 +0900

    Update 기능개발.java

--- 4주차/26일차/기능개발.java
+++ 4주차/26일차/기능개발.java
@@ -10,3 +10,26 @@ class Solution {
         return Arrays.stream(answer).filter(i -> i != 0).toArray();
     }
 }
+//
+import java.util.List;
+import java.util.ArrayList;
+class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+        int max = -1;
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0; i < progresses.length; i++){
+            int temp = 100 - progresses[i];
+            temp = (int)Math.ceil(temp / (double)speeds[i]);
+            if(temp <= max){
+                int size = list.remove(list.size() - 1);
+                list.add(size + 1);
+            }
+            else{
+                max = temp;
+                list.add(1);
+            }
+        }
+        
+        return list.stream().mapToInt(i -> i).toArray();
+    }
+}
