6e4f31ad56fe23058a0ef73b8bd0a954637b5f0e
css04146 <70522531+css04146@users.noreply.github.com>
2023-12-19 13:50:42 +0900

    Create 소수 찾기.java

---
+++ 6주차/38일차/소수 찾기.java
@@ -0,0 +1,37 @@
+import java.util.Set;
+import java.util.HashSet;
+class Solution {
+    int answer = 0;
+    Set<Integer> set = new HashSet<>();
+    public int solution(String numbers) {
+        makeNum("", numbers);
+        findPrime(set);
+        return answer;
+    }
+    
+    public void findPrime(Set<Integer> set){
+        for(int i : set){
+            if(isPrime(i)) answer++;
+        }
+    }
+    public void makeNum(String num, String words){
+        if(!num.equals("")) set.add(toi(num));
+        for(int i = 0; i < words.length(); i++){
+            makeNum(num + words.charAt(i),
+                   words.substring(0, i) + words.substring(i + 1));
+        }
+    }
+    
+    public boolean isPrime(int n){
+        if(n <= 1) return false;
+        if(n == 2) return true;
+        double sqrt = Math.sqrt(n);
+        for(int i = 2; i <= (int)sqrt; i++){
+            if(n % i == 0) return false;
+        }
+        return true;
+    }
+    public int toi(String s){
+        return Integer.parseInt(s);
+    }
+}
