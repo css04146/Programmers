685083c7f7bc6202cec0c49b2d8bf904f3e30008
css04146 <70522531+css04146@users.noreply.github.com>
2023-12-21 15:14:19 +0900

    Create 스킬트리.java

---
+++ 6주차/40일차/스킬트리.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(String skill, String[] skill_trees) {
+        int answer = 0;
+        for(String skill_tree : skill_trees){
+            skill_tree = skill_tree.replaceAll("[^" + skill + "]", "");
+            if(skill.indexOf(skill_tree) == 0) answer++;
+        }
+        return answer;
+    }
+}
+//
