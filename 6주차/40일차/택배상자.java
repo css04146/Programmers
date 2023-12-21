583be44a9c72543ad869ba8b376d64f57b5e6642
css04146 <70522531+css04146@users.noreply.github.com>
2023-12-21 16:01:18 +0900

    Create 택배상자.java

---
+++ 6주차/40일차/택배상자.java
@@ -0,0 +1,18 @@
+import java.util.Stack;
+class Solution {
+    public int solution(int[] order) {
+        int answer = 0, idx = 0;
+        Stack<Integer> st = new Stack<>();
+        for(int i = 1; i <= order.length; i++){
+            st.push(i);
+            while(!st.isEmpty() && st.peek() == order[idx]){
+                if(st.peek() == order[idx]){
+                    st.pop();
+                    answer++;
+                    idx++;
+                }
+            }
+        }
+        return answer;
+    }
+}
