64a2af22cc996b3b2617ab2d315384d541f769bd
css04146 <70522531+css04146@users.noreply.github.com>
2023-12-21 15:17:56 +0900

    Update 스킬트리.java

--- 6주차/40일차/스킬트리.java
+++ 6주차/40일차/스킬트리.java
@@ -9,3 +9,19 @@ class Solution {
     }
 }
 //
+import java.util.List;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.ArrayList;
+class Solution {
+    public int solution(String skill, String[] skill_trees) {
+        List<String> list = new ArrayList<>(Arrays.asList(skill_trees));
+        Iterator<String> iter = list.iterator();
+        while(iter.hasNext()){
+            if(skill.indexOf(iter.next().replaceAll("[^" + skill + "]", "")) != 0){
+                iter.remove();
+            }
+        }
+        return list.size();
+    }
+}
