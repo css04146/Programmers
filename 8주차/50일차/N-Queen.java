251208f6c96a7642ddf994aa129d0feea7390e03
css04146 <70522531+css04146@users.noreply.github.com>
2024-01-02 18:31:50 +0900

    Create N-Queen.java

---
+++ 8주차/50일차/N-Queen.java
@@ -0,0 +1,28 @@
+class Solution {
+    int[] arr;
+    int answer = 0;
+    public int solution(int n) {
+        arr = new int[n];
+        nQueen(0, n);
+        return answer;
+    }
+    public void nQueen(int depth, int n){
+        if(depth == n){
+            answer++;
+            return;
+        }
+        for(int i = 0; i < n; i++){
+            arr[depth] = i;
+            if(Possibility(depth)){
+                nQueen(depth + 1, n);
+            }
+        }
+    }
+    public boolean Possibility(int col){
+        for(int i = 0; i < col; i++){
+            if(arr[i] == arr[col]) return false;
+            else if(Math.abs(i - col) == Math.abs(arr[i] - arr[col])) return false;
+        }
+        return true;
+    }
+}
