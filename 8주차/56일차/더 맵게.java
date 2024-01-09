3d8c4d6691feede674b00d0a3a2e5438ee974303
css04146 <70522531+css04146@users.noreply.github.com>
2024-01-09 17:26:35 +0900

    Create 더 맵게.java

---
+++ 8주차/56일차/더 맵게.java
@@ -0,0 +1,15 @@
+import java.util.Queue;
+import java.util.PriorityQueue;
+class Solution {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        Queue<Integer> q = new PriorityQueue<>();
+        for(int s : scoville) q.add(s);
+        while(q.peek() < K){
+            if(q.size() == 1) return -1;
+            q.add(q.poll() + q.poll() * 2);
+            answer++;
+        }
+        return answer;
+    }
+}
