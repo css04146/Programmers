bebf73a326be724a647aa020733ba9642574c76b
css04146 <70522531+css04146@users.noreply.github.com>
2024-01-09 17:00:08 +0900

    Update 가장 큰 수.java

--- 8주차/56일차/가장 큰 수.java
+++ 8주차/56일차/가장 큰 수.java
@@ -18,3 +18,21 @@ class Solution {
         return String.valueOf(n);
     }
 }
+//
+import java.util.Arrays;
+import java.util.Comparator;
+class Solution {
+    public String solution(int[] numbers) {
+        String answer = "";
+        int len = numbers.length;
+        String[] str = new String[len];
+        for(int i = 0; i < len; i++) str[i] = tos(numbers[i]);
+        Arrays.sort(str, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
+        for(String s : str) answer += s;
+        if(answer.startsWith("0")) return "0";
+        return answer;
+    }
+    public String tos(int n){
+        return String.valueOf(n);
+    }
+}
