f36e09afac3256679c0571c10d210d91652e16cd
css04146 <70522531+css04146@users.noreply.github.com>
2024-01-11 16:22:07 +0900

    Update 문자열 다루기 기본.java

--- 9주차/58일차/문자열 다루기 기본.java
+++ 9주차/58일차/문자열 다루기 기본.java
@@ -6,3 +6,17 @@ class Solution {
         return answer;
     }
 }
+//
+class Solution {
+    public boolean solution(String s) {
+        if(s.length() == 4 || s.length() == 6){
+            try{
+                int n = Integer.parseInt(s);
+                return true;
+            } catch(NumberFormatException e){
+                return false;
+            }
+        }
+        else return false;
+    }
+}
