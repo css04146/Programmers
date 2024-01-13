dfd23b0230f35f18706e74d46b02cfb4f412e55a
css04146 <70522531+css04146@users.noreply.github.com>
2024-01-13 16:20:34 +0900

    Create 점프와 순간 이동.java

---
+++ 9주차/60일차/점프와 순간 이동.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int ans = 0;
+        while(n != 0){
+            ans = n % 2 == 0 ? ans : ans + 1;
+            n = n % 2 == 0 ? n / 2 : n - 1;
+        }
+        return ans;
+    }
+}
