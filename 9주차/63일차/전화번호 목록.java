4dd327ea8514083a242014a7464627b806c5a8e4
css04146 <70522531+css04146@users.noreply.github.com>
2024-01-17 12:58:04 +0900

    Create 전화번호 목록.java

---
+++ 9주차/63일차/전화번호 목록.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+class Solution {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        Arrays.sort(phone_book);
+        int len = phone_book.length;
+        for(int i = 0; i < len - 1; i++){
+            if(phone_book[i + 1].startsWith(phone_book[i])){
+                return false;
+            }
+        }
+        return answer;
+    }
+}
