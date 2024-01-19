8f953e492b7e1723d1ede44dc572a6fa28bf2e83
css04146 <70522531+css04146@users.noreply.github.com>
2024-01-19 15:11:48 +0900

    Create 숫자 변환하기.java

---
+++ 10주차/65일차/숫자 변환하기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int x, int y, int n) {
+        int[] arr = new int[y + 1];
+        java.util.Arrays.fill(arr, y);
+        arr[x] = 0;
+        for(int i = x; i <= y; i++){
+            if(i + n <= y) arr[i + n] = Math.min(arr[i] + 1, arr[i + n]);
+            if(i * 2 <= y) arr[i * 2] = Math.min(arr[i] + 1, arr[i * 2]);
+            if(i * 3 <= y) arr[i * 3] = Math.min(arr[i] + 1, arr[i * 3]);
+        }
+        int answer = arr[y];
+        answer = answer == y ? -1 : answer;
+        return answer;
+    }
+}
