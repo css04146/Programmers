9c067889b25f15d5176142bc882e060179ffc143
css04146 <70522531+css04146@users.noreply.github.com>
2024-03-14 13:45:58 +0900

    Update 약수의 합.java

--- 13주차/85일차/약수의 합.java
+++ 13주차/85일차/약수의 합.java
@@ -5,6 +5,13 @@ class Solution {
         return answer;
     }
 }
+import java.util.*;
+import java.util.stream.*;
+class Solution {
+    public int solution(int n) {
+        return IntStream.range(1, n + 1).filter(i -> n % i == 0).sum();
+    }
+}
 /* 파이썬
 def solution(n):
     return sum(i for i in range(1, n + 1) if n % i == 0)
