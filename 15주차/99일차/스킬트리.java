f267116a62f91707385f4c6e02d3c51e44508b16
css04146 <70522531+css04146@users.noreply.github.com>
2024-05-01 17:11:47 +0900

    Create 스킬트리.java

---
+++ 15주차/99일차/스킬트리.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(String skill, String[] skill_trees) {
+        int answer = skill_trees.length;
+        for(String skill_tree : skill_trees){
+            for(int i = 0; i < skill.length() - 1; i++){
+                String c1 = String.valueOf(skill.charAt(i));
+                String c2 = String.valueOf(skill.charAt(i + 1));
+                if(!skill_tree.contains(c1)
+                && skill_tree.contains(c2)){
+                    answer--;
+                    break;
+                }
+                if(skill_tree.contains(c1)
+                && skill_tree.contains(c2)){
+                    if(skill_tree.indexOf(c1) > skill_tree.indexOf(c2)){
+                        answer--;
+                        break;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+}
